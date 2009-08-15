33f68a19b9932339a7803426d749892a10282c6a
Alan Gutierrez <alan@blogometer.com>
2009-08-15 05:20:57 -0500

    Ideas for a domain-specific language.
    - A domain specific language that would define the commands available to the
      project.

---
+++ src/test/java/com/goodworkalan/mix/ModuleTest.java
@@ -0,0 +1,28 @@
+package com.goodworkalan.mix;
+
+public class ModuleTest extends BuildModule {
+    public void foo(Build build) {
+//        build
+//            .task("compile", "profile")
+//                .run(PreOperation.class).end()
+//                .run(Javac.class).end()
+//                .run(PostOperation.class).end()
+//                .end();
+//        build
+//            .task("publish")
+//                .run(MyPublishing.class).end()
+//                .end();
+//        build
+//            .task("api:public")
+//                .run(Javadoc.class)
+//                    .visibility(Visibility.PROTECTED)
+//                    .output("target/api/public")
+//                    .end()
+//                .end();
+//        build
+//            .task("api:private")
+//        build.tool(Javac.class).warn(false).end();
+//        build.tool(Cobertura.class).files(find("src/main/java").exclude(find("")).end();
+//        build.tool()
+    }
+}
