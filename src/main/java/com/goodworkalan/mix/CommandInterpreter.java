170b54020a3cead7f5813ab4445dde9d8b6503c0
Alan Gutierrez <alan@blogometer.com>
2009-08-15 05:12:16 -0500

    Bootstrap complete.
    - Now able to bootstrap from the Mix.java bootstrap class.

--- src/main/java/com/goodworkalan/mix/CommandInterpreter.java
+++ src/main/java/com/goodworkalan/mix/CommandInterpreter.java
@@ -1,10 +1,22 @@
 package com.goodworkalan.mix;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import mix.Mix;
+
 /**
  * Make sense of the command line.
  *
  * @author Alan Gutierrez
  */
-public class CommandInterpreter
+public class CommandInterpreter implements Mix.Arguable
 {
+    public void main(String[] arguments)
+    {
+        Map<String, String> parameters = new LinkedHashMap<String, String>();
+        for (int i = 0, stop = arguments.length; i < stop; i++) {
+            System.out.println(arguments[i]);
+        }
+    }
 }
