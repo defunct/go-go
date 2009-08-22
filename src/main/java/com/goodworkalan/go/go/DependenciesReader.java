8b97a2426a6c20946096c81746be9493a59fe982
Alan Gutierrez <alan@blogometer.com>
2009-08-21 23:37:24 -0500

    Implemented DependenciesReader.

--- src/main/java/com/goodworkalan/go/go/DependenciesReader.java
+++ src/main/java/com/goodworkalan/go/go/DependenciesReader.java
@@ -5,9 +5,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,17 +21,23 @@ import java.util.Map;
  */
 public class DependenciesReader {
     /** Map of respoitory names to repository classes. */
-    private final static Map<String, Boolean> repositories = new HashMap<String, Boolean>();
+    private final static Map<String, Class<? extends Repository>> repositories = new HashMap<String, Class<? extends Repository>>();
     
     static {
-        repositories.put("flat", true);
-        repositories.put("maven", true);
+        repositories.put("flat", FlatRepository.class);
+        repositories.put("maven", MavenRepository.class);
     };
+    
+    private final Repository repository;
+    
+    private final List<Artifact> artifacts = new ArrayList<Artifact>();
 
     public DependenciesReader(File file) {
         if (!file.exists()) {
             throw new GoException(0);
         }
+        Class<? extends Repository> repositoryClass = null;
+        URI uri = null;
         try {
             BufferedReader lines;
             try {
@@ -36,19 +45,18 @@ public class DependenciesReader {
             } catch (FileNotFoundException e) {
                 throw new GoException(0, e);
             }
-            String found = null;
             String line = lines.readLine();
-            while (found == null && (line = lines.readLine()) != null) {
+            while (repositoryClass == null && (line = lines.readLine()) != null) {
                 line = line.trim();
                 if (!(line.startsWith("#") || line.equals(""))) {
                     String[] repository = line.split("\\s+");
                     if (repository.length != 2) {
                         throw new GoException(0);
                     }
-                    if (!repositories.containsKey(repository[0])) {
+                    repositoryClass = repositories.get(repository[0]);
+                    if (repositoryClass == null) {
                         throw new GoException(0);
                     }
-                    URI uri;
                     try {
                         uri = new URI(repository[1]);
                     } catch (URISyntaxException e) {
@@ -59,20 +67,47 @@ public class DependenciesReader {
                     }
                 }
             }
-            if (found == null) {
+            if (repositoryClass == null) {
                 throw new GoException(0);
             }
             while ((line = lines.readLine()) != null) {
                 line = line.trim();
                 if ((!line.startsWith("#") || line.equals(""))) {
-                    String[] dependency = line.split("\\s+");
-                    if (dependency.length != 3) {
+                    String[] artifact = line.split("\\s+");
+                    if (artifact.length != 3) {
                         throw new GoException(0);
                     }
+                    artifacts.add(new Artifact(artifact[0], artifact[1], artifact[2]));
                 }
             }
         } catch (IOException e) {
             throw new GoException(0, e);
         }
+        Constructor<?> constructor;
+        try {
+            constructor = repositoryClass.getConstructor(URI.class);
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new GoException(0, e);
+        }
+        try {
+            repository = (Repository) constructor.newInstance(uri);
+            // TODO WOW! I never noticed all this before! Need to write about it.
+        } catch (IllegalArgumentException e) {
+            throw new GoException(0, e);
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new GoException(0, e);
+        }
+    }
+    
+    public Repository getRepository() {
+        return repository;
+    }
+    
+    public List<Artifact> getArtifacts() {
+        return artifacts;
     }
 }
