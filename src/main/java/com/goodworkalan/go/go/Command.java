30a70bba8d813a79d8dba2cd653596e2f4f8e25f
Alan Gutierrez <alan@blogometer.com>
2009-08-21 19:25:17 -0500

    Javadoc for Command.

--- src/main/java/com/goodworkalan/go/go/Command.java
+++ src/main/java/com/goodworkalan/go/go/Command.java
@@ -6,6 +6,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation applied to {@link Task} Beans to specify an command name other
+ * than the one derived from the class name.
+ * 
+ * @author Alan Gutierrez
+ */
 @Documented
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
