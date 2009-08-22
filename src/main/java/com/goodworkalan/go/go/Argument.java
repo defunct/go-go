d18adf7f910f1983cb44b98caecbdf91f5992786
Alan Gutierrez <alan@blogometer.com>
2009-08-21 19:22:30 -0500

    Javadoc for Argument.

--- src/main/java/com/goodworkalan/go/go/Argument.java
+++ src/main/java/com/goodworkalan/go/go/Argument.java
@@ -1,10 +1,20 @@
 package com.goodworkalan.go.go;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation applied to the setter methods of argument properties for
+ * {@link Task} Beans. The property must be either a <code>String</code>
+ * property or an object that with a constructor that takes a single
+ * <code>String</code> argument.
+ * 
+ * @author Alan Gutierrez
+ */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Argument {
