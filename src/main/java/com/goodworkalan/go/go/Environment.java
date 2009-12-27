ab4eba40983b4c0a76c049e6778a8d7907b75a30
Alan Gutierrez <alan@blogometer.com>
2009-12-26 21:46:10 -0600

    Created a formatted error output.method.

--- src/main/java/com/goodworkalan/go/go/Environment.java
+++ src/main/java/com/goodworkalan/go/go/Environment.java
@@ -1,5 +1,9 @@
 package com.goodworkalan.go.go;
 
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
 /**
  * The environment in which a task is executed.
  * 
@@ -29,5 +33,40 @@ public class Environment {
         this.io = io;
         this.part = part;
         this.executor = executor;
+    }
+    
+    public void error(String message, Object...arguments) {
+        error(0, part.getTaskClass(), message, arguments);
+    }
+    
+    public void error(int indent, String message, Object...arguments) {
+        error(indent, part.getTaskClass(), message, arguments);
+    }
+    public void error(Class<?> context, String token, Object...arguments) {
+        error(0, context, token, arguments);
+    }
+
+    public void error(int indent, Class<?> context, String token, Object...arguments) {
+        String className = context.getCanonicalName();
+        int index = className.lastIndexOf('.');
+        if (index > -1) {
+            className = className.substring(index + 1);
+        }
+        String key = className + "/" + token; 
+        ResourceBundle bundle;
+        try {
+            bundle = ResourceBundle.getBundle(context.getPackage().getName() + ".stderr", Locale.getDefault(), Thread.currentThread().getContextClassLoader());
+        } catch (MissingResourceException e) {
+            bundle = ResourceBundle.getBundle(Environment.class.getPackage().getName() + ".stderr");
+            key = "Environment/bundle.missing";
+        }
+        while (indent-- != 0) {
+            io.err.print("    ");
+        }
+        try {
+            io.err.println(String.format(bundle.getString(key), arguments));
+        } catch (MissingResourceException e) {
+            error(0, Environment.class, "message.missing");
+        }
     }
 }
