f8cb439a618dc97bd0d560ebad19556576698540
Alan Gutierrez <alan@blogometer.com>
2010-06-09 18:13:40 -0500

    Update Mix build after refactor of Mix.

--- src/mix/java/com/goodworkalan/go/go/mix/GoProject.java
+++ src/mix/java/com/goodworkalan/go/go/mix/GoProject.java
@@ -2,7 +2,7 @@ package com.goodworkalan.go.go.mix;
 
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
-import com.goodworkalan.mix.builder.JavaProject;
+import com.goodworkalan.mix.cookbook.JavaProject;
 
 /**
  * Build definition for Jav-a-Go-Go.
@@ -20,23 +20,17 @@ public class GoProject implements ProjectModule {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.go-go/go-go/0.1.4.15")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.reflective/reflective-setter/0.+1")
-                        .include("com.github.bigeasy.ilk/ilk/0.+1")
-                        .include("com.github.bigeasy.danger/danger/0.+1")
-                        .include("com.github.bigeasy.retry/retry/0.+1")
-                        .include("com.github.bigeasy.class-boxer/class-boxer/0.+1")
-                        .include("com.github.bigeasy.infuse/infuse/0.+1")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("com.github.bigeasy.comfort-io/comfort-io/0.+1.1")
-                        .include("org.slf4j/slf4j-log4j12/1.4.2")
-                        .include("org.testng/testng-jdk15/5.10")
-                        .include("org.mockito/mockito-core/1.6")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.reflective/reflective-setter/0.+1")
+                    .production("com.github.bigeasy.ilk/ilk/0.+1")
+                    .production("com.github.bigeasy.danger/danger/0.+1")
+                    .production("com.github.bigeasy.retry/retry/0.+1")
+                    .production("com.github.bigeasy.class-boxer/class-boxer/0.+1")
+                    .production("com.github.bigeasy.infuse/infuse/0.+1")
+                    .development("com.github.bigeasy.comfort-io/comfort-io/0.+1.1")
+                    .development("org.slf4j/slf4j-log4j12/1.4.2")
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
